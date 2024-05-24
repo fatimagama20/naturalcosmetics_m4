a85b2bafb91d7eb73aabb4a206c27d85b5422943
fatimagama20 <familygama20@gmail.com>
2024-05-24 16:19:45 +0000

    Added adjust bag and remove from bag items and calculated subtotal

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,18 +9,42 @@ def bag_contents(request):
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
+    
 
+    # for item_id, item_data in bag.items():
+    #     product = get_object_or_404(Product, pk=item_id)
+    #     for size, quantity in item_data['items_by_size'].items():
+    #         total += quantity * product.price
+    #         product_count += quantity
+    #         bag_items.append({
+    #             'item_id': item_id,
+    #             'quantity': quantity,
+    #             'product': product,
+    #             'size': size,
+    #         })
+    
+    
     for item_id, item_data in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        for size, quantity in item_data['items_by_size'].items():
-            total += quantity * product.price
-            product_count += quantity
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
             bag_items.append({
                 'item_id': item_id,
-                'quantity': quantity,
+                'quantity': item_data,
                 'product': product,
-                'size': size,
             })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
     
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
