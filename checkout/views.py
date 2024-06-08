3d413bd12fa0add34ead86543ab25887fe9f7d2e
fatimagama20 <familygama20@gmail.com>
2024-06-08 17:52:03 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
+        # pk_test_51PPSjbDAbnDBv5gxnCeOfl1XQBWJMPEbD8kzCCo9JvsRzrjwa0FOHn2xWyDrfnnPZBtBvndPtdF3MEJ4kLtcKumx008jNsNcYc
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
