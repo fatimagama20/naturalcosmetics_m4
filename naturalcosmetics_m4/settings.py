5e2523e02127a5f7c07f7c0f942588fb0c324e10
Fatima Gama <familygama20@gmail.com>
2024-08-09 16:29:10 +0000

    Removed secret key and set debug

--- naturalcosmetics_m4/settings.py
+++ naturalcosmetics_m4/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bipeoa-5vw7f6f)6nuos*kjh5y!l0n)2=04+(^ik=wlu7*3-*)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 # ALLOWED_HOSTS = ['8000-fatimagama2-naturalcosm-cz32zcdmx8n.ws.codeinstitute-ide.net']
 ALLOWED_HOSTS = ['naturalcosmetics.herokuapp.com','localhost']
