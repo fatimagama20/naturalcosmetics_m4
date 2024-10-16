dc9619606f56650b4c4ae2a85a21a89666fea68d
Fatima Gama <familygama20@gmail.com>
2024-10-16 13:33:30 +0000

    Contact form added

--- contact/views.py
+++ contact/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 from .forms import ContactForm
+from .models import Contact
 
 def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('success')
+            messages.success(request, 'Email sent successfully!')
+            return redirect(reverse('contact'))
     else:
         form = ContactForm()
-    return render(request, 'contact/contact.html', {'form': form})
+    
+    template = 'contact/contact.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
