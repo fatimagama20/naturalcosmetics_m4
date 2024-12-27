b7ca6646bda367bc8da4df2083c3069e42dfeeff
Fatima Gama <familygama20@gmail.com>
2024-12-27 20:51:20 +0000

    Added mailing list feature

---
+++ mailinglist/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .forms import SubscriberForm
+
+def subscribe(request):
+    success = False
+    if request.method == 'POST':
+        form = SubscriberForm(request.POST)
+        if form.is_valid():
+            form.save()
+            success = True
+    else:
+        form = SubscriberForm()
+    return render(request, 'home/index.html', {'form': form, 'success': success})
